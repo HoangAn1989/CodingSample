69bbe940b80bf3bd55676712afb280510d50d45f
HoangAn1989 <bri4ndinh@gmail.com>
2016-03-17 13:14:45 -0700

    DriverLicense

---
+++ DriverLicense/Main.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace DriverLicense
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refeshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.returnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnTest = new System.Windows.Forms.Panel();
+            this.rdbAnswer1 = new System.Windows.Forms.RadioButton();
+            this.tbQuestion = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.llbSkip = new System.Windows.Forms.LinkLabel();
+            this.rdbAnswer2 = new System.Windows.Forms.RadioButton();
+            this.rdbAnswer3 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            this.pnTest.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(916, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CALIFORNIA DMV DRIVER\'S LICENSE PRACTICE TEST";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(19, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(935, 18);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.adminToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(966, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refeshToolStripMenuItem,
+            this.returnToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // refeshToolStripMenuItem
+            // 
+            this.refeshToolStripMenuItem.Name = "refeshToolStripMenuItem";
+            this.refeshToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.refeshToolStripMenuItem.Text = "Refesh";
+            // 
+            // returnToolStripMenuItem
+            // 
+            this.returnToolStripMenuItem.Name = "returnToolStripMenuItem";
+            this.returnToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.returnToolStripMenuItem.Text = "Return";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(109, 22);
+            this.toolStripMenuItem2.Text = ".";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // adminToolStripMenuItem
+            // 
+            this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.adminToolStripMenuItem.Text = "Admin";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // pnTest
+            // 
+            this.pnTest.Controls.Add(this.rdbAnswer3);
+            this.pnTest.Controls.Add(this.rdbAnswer2);
+            this.pnTest.Controls.Add(this.rdbAnswer1);
+            this.pnTest.Controls.Add(this.tbQuestion);
+            this.pnTest.Location = new System.Drawing.Point(12, 95);
+            this.pnTest.Name = "pnTest";
+            this.pnTest.Size = new System.Drawing.Size(942, 347);
+            this.pnTest.TabIndex = 4;
+            this.pnTest.Visible = false;
+            // 
+            // rdbAnswer1
+            // 
+            this.rdbAnswer1.AutoSize = true;
+            this.rdbAnswer1.Location = new System.Drawing.Point(62, 140);
+            this.rdbAnswer1.Name = "rdbAnswer1";
+            this.rdbAnswer1.Size = new System.Drawing.Size(85, 17);
+            this.rdbAnswer1.TabIndex = 2;
+            this.rdbAnswer1.TabStop = true;
+            this.rdbAnswer1.Text = "radioButton1";
+            this.rdbAnswer1.UseVisualStyleBackColor = true;
+            // 
+            // tbQuestion
+            // 
+            this.tbQuestion.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.tbQuestion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbQuestion.Location = new System.Drawing.Point(9, 13);
+            this.tbQuestion.Multiline = true;
+            this.tbQuestion.Name = "tbQuestion";
+            this.tbQuestion.ReadOnly = true;
+            this.tbQuestion.Size = new System.Drawing.Size(922, 103);
+            this.tbQuestion.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnStart.Location = new System.Drawing.Point(392, 463);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(152, 42);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnNext.Location = new System.Drawing.Point(21, 463);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(152, 42);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "NEXT";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Visible = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // llbSkip
+            // 
+            this.llbSkip.AutoSize = true;
+            this.llbSkip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.llbSkip.Location = new System.Drawing.Point(209, 485);
+            this.llbSkip.Name = "llbSkip";
+            this.llbSkip.Size = new System.Drawing.Size(66, 20);
+            this.llbSkip.TabIndex = 6;
+            this.llbSkip.TabStop = true;
+            this.llbSkip.Text = "Skip  >>";
+            this.llbSkip.Visible = false;
+            // 
+            // rdbAnswer2
+            // 
+            this.rdbAnswer2.AutoSize = true;
+            this.rdbAnswer2.Location = new System.Drawing.Point(62, 206);
+            this.rdbAnswer2.Name = "rdbAnswer2";
+            this.rdbAnswer2.Size = new System.Drawing.Size(85, 17);
+            this.rdbAnswer2.TabIndex = 2;
+            this.rdbAnswer2.TabStop = true;
+            this.rdbAnswer2.Text = "radioButton1";
+            this.rdbAnswer2.UseVisualStyleBackColor = true;
+            // 
+            // rdbAnswer3
+            // 
+            this.rdbAnswer3.AutoSize = true;
+            this.rdbAnswer3.Location = new System.Drawing.Point(62, 277);
+            this.rdbAnswer3.Name = "rdbAnswer3";
+            this.rdbAnswer3.Size = new System.Drawing.Size(85, 17);
+            this.rdbAnswer3.TabIndex = 2;
+            this.rdbAnswer3.TabStop = true;
+            this.rdbAnswer3.Text = "radioButton1";
+            this.rdbAnswer3.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.ClientSize = new System.Drawing.Size(966, 517);
+            this.Controls.Add(this.llbSkip);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pnTest);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Driver\'s License Test (VietNamese)";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.pnTest.ResumeLayout(false);
+            this.pnTest.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refeshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem returnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Panel pnTest;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.RadioButton rdbAnswer1;
+        private System.Windows.Forms.TextBox tbQuestion;
+        private System.Windows.Forms.LinkLabel llbSkip;
+        private System.Windows.Forms.RadioButton rdbAnswer3;
+        private System.Windows.Forms.RadioButton rdbAnswer2;
+    }
+}
+
---
+++ DriverLicense/Main.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace DriverLicense
+{
+    public partial class Main : Form
+    {
+        SqlConnection cnn;
+        SqlDataAdapter adapter;
+        DataTable table = new DataTable();
+        public static Random _Random = new Random();
+        public ArrayList questionList;
+        public ArrayList realTest;
+        public int _count = 0;
+        public Main()
+        {
+            InitializeComponent();
+            cnn = new SqlConnection("server=FAMILY-PC; database=DriverLicenseTest; integrated security= true");
+            cnn.Open();
+            
+        }
+        private void connect()
+        {
+            cnn = new SqlConnection("server=FAMILY-PC; database=DriverLicenseTest; integrated security= true");
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int getIdQuestion;
+            pnTest.Visible = true;
+            btnStart.Visible = false;
+            btnNext.Visible = true;
+            llbSkip.Visible = true;
+            getAllQuestion();
+            getIdQuestion = (int) realTest[_count];
+            _count++;
+            tbQuestion.Text = getQuestion(getIdQuestion);
+            string answer1 = getAnswer_1(getIdQuestion);
+            rdbAnswer1.Text = answer1;
+            string answer2 = getAnswer_2(getIdQuestion);
+            rdbAnswer2.Text = answer2;
+            string answer3 = getAnswer_3(getIdQuestion);
+            rdbAnswer3.Text = answer3;
+        }      
+        
+
+        public void getAllQuestion()
+        {
+            connect();
+            adapter = new SqlDataAdapter("Select * from QUESTION", cnn);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            int countRow = table.Rows.Count;
+            questionList = new ArrayList(countRow);
+            for(int i=0; i<countRow; i++)
+            {                
+                questionList.Add(table.Rows[i][0]);
+                
+            }
+            
+            realTest = new ArrayList(25);
+            int getID;
+            for(int j=0; j<25; j++)
+            {
+                int numberRandom = _Random.Next(questionList.Count);
+                getID = (int)questionList[numberRandom];
+                realTest.Add(getID);
+                //questionList.Remove(numberRandom);
+            }
+        }
+
+        private String getQuestion(int id)
+        {
+            cnn = new SqlConnection("server=FAMILY-PC; database=DriverLicenseTest; integrated security= true");
+            String content;            
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "Select CONTENT_QUESTION from QUESTION where ID_QUESTION = @id";
+            command.CommandType = CommandType.Text;
+            command.Parameters.AddWithValue("@id", id);
+            command.Connection = cnn;
+            cnn.Open();
+            content = command.ExecuteScalar().ToString();
+            return content;
+        }
+
+        private String getAnswer_1(int id)
+        {
+            cnn = new SqlConnection("server=FAMILY-PC; database=DriverLicenseTest; integrated security= true");
+            String answer;
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "Select ANSWER1 from ANSWER_1 where IDQUESTION = @id";
+            command.CommandType = CommandType.Text;
+            command.Parameters.AddWithValue("@id", id);
+            command.Connection = cnn;
+            cnn.Open();
+            answer = command.ExecuteScalar().ToString();
+            return answer;
+        }
+
+        private String getAnswer_2(int id)
+        {
+            cnn = new SqlConnection("server=FAMILY-PC; database=DriverLicenseTest; integrated security= true");
+            String answer;
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "Select ANSWER2 from ANSWER_2 where IDQUESTION = @id";
+            command.CommandType = CommandType.Text;
+            command.Parameters.AddWithValue("@id", id);
+            command.Connection = cnn;
+            cnn.Open();
+            answer = command.ExecuteScalar().ToString();
+            return answer;
+        }
+
+        private String getAnswer_3(int id)
+        {
+            cnn = new SqlConnection("server=FAMILY-PC; database=DriverLicenseTest; integrated security= true");
+            String answer;
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "Select ANSWER3 from ANSWER_3 where IDQUESTION = @id";
+            command.CommandType = CommandType.Text;
+            command.Parameters.AddWithValue("@id", id);
+            command.Connection = cnn;
+            cnn.Open();
+            answer = command.ExecuteScalar().ToString();
+            return answer;
+        }       
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if(_count < 25)
+            {
+                int getIdQuestion;
+                getIdQuestion = (int)realTest[_count];
+                _count++;
+                tbQuestion.Text = getQuestion(getIdQuestion);
+                string answer1 = getAnswer_1(getIdQuestion);
+                rdbAnswer1.Text = answer1;
+                string answer2 = getAnswer_2(getIdQuestion);
+                rdbAnswer2.Text = answer2;
+                string answer3 = getAnswer_3(getIdQuestion);
+                rdbAnswer3.Text = answer3;
+            }
+            else if(_count == 25)
+            {
+                pnTest.Visible = false;
+                btnNext.Visible = false;
+                llbSkip.Visible = false;
+            }
+        }
+        
+    }
+}
